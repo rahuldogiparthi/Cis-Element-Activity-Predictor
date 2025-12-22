d084b3d97a66da318b0de4e3c8b96f56ff196c59
Rahul Dogiparthi <44081380+rahuldogiparthi@users.noreply.github.com>
2025-12-22 17:53:04 -0600

    Modify peak file input and library comments
    Updated peak file path and added comment for hg38.

--- scripts/Genome_Annotation.R
+++ scripts/Genome_Annotation.R
@@ -1,12 +1,13 @@
 # Load Libraries
 library(ChIPseeker)
-library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+library(TxDb.Hsapiens.UCSC.hg19.knownGene) # Change to library(TxDb.Hsapiens.UCSC.hg38.knownGene) if using hg38
 library(org.Hs.eg.db)
 
 # Input peak files
-peak <- readPeakFile("Kit_Activated_Cistrome_TF_selected_hg19_only_coordinates.bed")
+peak <- readPeakFile("data/Kit_Activated_Peaks.bed") # Change the peak file accordingly for different conditions
 txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
 peakAnno <- annotatePeak(peak, TxDb=txdb, tssRegion=c(-1000, 1000))
 
 # Plot Pie chart showing genome compostion
 plotAnnoPie(peakAnno)
+
