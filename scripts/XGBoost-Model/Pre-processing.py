1c8436b3c722b0ffeb7f007232fa85de8270305b
Rahul Dogiparthi <44081380+rahuldogiparthi@users.noreply.github.com>
2025-12-23 16:49:06 -0500

    Update Pre-processing.py

--- scripts/XGBoost-Model/Pre-processing.py
+++ scripts/XGBoost-Model/Pre-processing.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from imblearn.over_sampling import SMOTE
+
 
 # Load datasets
 kit_activated = pd.read_table("data/Kit_Activated_Training.txt")
@@ -26,16 +24,3 @@ df = df.sample(frac=1, random_state=42).reset_index(drop=True)
 X = df.drop(columns=["Label"])
 y = df["Label"]
 
-# Handle class imbalance using SMOTE
-smote = SMOTE(sampling_strategy="auto", random_state=42, k_neighbors=5)
-X_balanced, y_balanced = smote.fit_resample(X, y)
-
-# Split into train/test sets
-X_train, X_test, y_train, y_test = train_test_split(X_balanced, y_balanced, test_size=0.2, stratify=y_balanced, random_state=42)
-
-# Scale features
-scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
-
-# print(f"Data Loaded & Preprocessed. Train shape: {X_train.shape}, Test shape: {X_test.shape}")
