407da27e55f664de44be93ce47668e25769ba255
Rahul Dogiparthi <44081380+rahuldogiparthi@users.noreply.github.com>
2025-12-25 15:48:20 -0600

    Clarify dataset sources

--- scripts/gchromVAR_Analysis.R
+++ scripts/gchromVAR_Analysis.R
@@ -1,6 +1,7 @@
 # Code template obtained from https://rdrr.io/github/caleblareau/gchromVAR/f/vignettes/gchromVAR_vignette.Rmd for gChromVAR analysis
-
+# The input datasets and analysis have been performed with the hg19 assembly to associate with the gchromVAR analysis.
 # Load the libraries
+
 library(GenomicRanges)
 library(Rsubread)
 library(gtools)
@@ -21,13 +22,12 @@ library(motifmatchr)
 library(JASPAR2024)
 library(pheatmap)
 
-# Load .narrowPeak files
+# Load .narrowPeak files. Peaks files are available at GSE314033
 peaks <- dir("data/narrowpeaks/", pattern = "*.narrowPeak", full.names = TRUE)
 peaks<-mixedsort(peaks)
-
 myPeaks <- lapply(peaks, ChIPQC:::GetGRanges, simple = TRUE)
-
 names(myPeaks)<-c("ScrmSCF_1","ScrmSCF_2","ScrpSCF_1","ScrpSCF_2")
+
 #Group<-factor(c(rep("ScrmSCF",1), rep("ScrpSCF",1)))
 
 # Generate consensus counts for the peaks
@@ -47,9 +47,9 @@ occurrences <- elementMetadata(consensusToCount) %>% as.data.frame %>% dplyr::se
 table(occurrences) %>% rev %>% cumsum
 consensusToCount <- consensusToCount[occurrences >= 2, ]
 
-# Feature Counts from bams
-setwd("data/bams/")
-bamsToCount <- dir("./", full.names = TRUE, pattern = "*.\\.bam$")
+# Feature Counts from bams. Raw fastq files are available at GSE314033
+
+bamsToCount <- dir("data/bams/", full.names = TRUE, pattern = "*.\\.bam$")
 bamsToCount<-mixedsort(bamsToCount)
 bamsToCount
 regionsToCount <- data.frame(GeneID = paste("ID", seqnames(consensusToCount),
@@ -59,6 +59,7 @@ fcResults <- featureCounts(bamsToCount, annot.ext = regionsToCount, isPairedEnd 
                            countMultiMappingReads = FALSE, maxFragLength = 100)
 myCounts <- fcResults$counts
 
+# gChromVAR analysis                                      
 # Mapping counts to Hematopoietic SNP regions
 
 non_overlapping_regions_df<-non_overlapping_region_counts(consensus_counts_generator)
@@ -72,6 +73,7 @@ ukbb_wDEV <- computeWeightedDeviations(myCounts2, ukbb)
 zscoredf <- reshape2::melt(t(assays(ukbb_wDEV)[["z"]]))
 zscoredf[,2] <- gsub("_PP001", "", zscoredf[,2])
 colnames(zscoredf) <- c("Sample", "Trait", "Z-score")
+write.csv(zscoredf,file="gchromVAR_analysis_table_zscores.csv", row.names=FALSE)
 
 # Visualizing the accessibility variation between conditions at SNPs by Heatmap                                          
 df_avg <- zscoredf %>%
@@ -90,6 +92,6 @@ rownames(mat) <- mat$Trait
 mat$Trait <- NULL
 mat <- as.matrix(mat)
 
-pdf("Fig_heatmap_zscores.pdf", width = 6, height = 8)
+pdf("Fig1_gChromVAR_heatmap_zscores.pdf", width = 6, height = 8)
 pheatmap(mat)
 dev.off()
