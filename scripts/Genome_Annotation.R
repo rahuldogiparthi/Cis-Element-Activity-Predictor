380c7a1f83c57ba193a8444ad3cead4688b251d5
Rahul Dogiparthi <44081380+rahuldogiparthi@users.noreply.github.com>
2025-12-26 14:54:27 -0600

    Update Genome_Annotation.R

--- scripts/Genome_Annotation.R
+++ scripts/Genome_Annotation.R
@@ -4,12 +4,13 @@ library(TxDb.Hsapiens.UCSC.hg19.knownGene) # Change to library(TxDb.Hsapiens.UCS
 library(org.Hs.eg.db)
 
 # Input peak files
-peak <- readPeakFile("data/Kit_Activated_Peaks.bed") # Change the peak file accordingly for different conditions
+peak <- readPeakFile("data/PlotAnnoPie/Kit_Activated_Peaks.bed") # Change the peak file accordingly for different conditions
 txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
 peakAnno <- annotatePeak(peak, TxDb=txdb, tssRegion=c(-1000, 1000))
 
 # Plot Pie chart showing genome compostion
-pdf("Peaks_Annotation.pdf")
+pdf("Kit_Activated_Peaks_AnnoPieChart.pdf")
 plotAnnoPie(peakAnno)
 dev.off()
+
 
